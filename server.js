271f804c5f2f299ed61f7fd614256004d668a2f3
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2024-09-04 09:03:21 +0900

    commit

--- server.js
+++ server.js
@@ -113,4 +113,7 @@ app.get("/resetComments", async (req, resp) => {
     }
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
+//server.listen(port, () => console.log(`Listening on port ${port}!`));
